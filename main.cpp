bbf4e580779a7fbb3c87418b7ea9c748afa5398f
yzu1091632 <147010584+yzu1091632@users.noreply.github.com>
2023-10-05 09:44:53 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
+/***
+ *  hw20231005
+ *  @author: 1091632
+ *  @name: 方國軒
+***/
 #include <iostream>
 using namespace std;
+
 int Fib(int n)
 {
-    return 1;
+    if(n == 1 || n == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        return Fib(n - 1) + Fib(n - 2);
+    }
 }
 int main()
 {
